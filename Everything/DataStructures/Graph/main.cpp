3b4202bcce9cf89ecde4fb14f10472d2dde94639
Michał Lejza <101932740+MichalLejza@users.noreply.github.com>
2023-11-29 12:35:55 +0100

    Update main.cpp

--- Everything/DataStructures/Graph/main.cpp
+++ Everything/DataStructures/Graph/main.cpp
@@ -5,14 +5,14 @@ auto main() -> int
 {
     auto *graph = new Graph::WeightedGraph<std::string>();
 
-    graph ->addNode("A", "B", 9);
-    graph ->addNode("B", "C", 12);
-    graph ->addNode("D", "C", 8);
-    graph ->addNode("E", "D", 0);
-    graph ->addNode("A", "E", 3);
+    graph -> addNode("A", "B", 9);
+    graph -> addNode("B", "C", 12);
+    graph -> addNode("D", "C", 8);
+    graph -> addNode("E", "D", 0);
+    graph -> addNode("A", "E", 3);
     graph -> printGraph();
-    graph ->deleteNode("A");
-    graph->printGraph();
+    graph -> deleteNode("A");
+    graph -> printGraph();
 
     return 0;
 }

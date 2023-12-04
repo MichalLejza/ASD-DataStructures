1c413a129a1a7a4222ca2fcb4c5f044eeb42c0ec
Michał Lejza <101932740+MichalLejza@users.noreply.github.com>
2023-12-04 18:54:21 +0100

    Update Algorithms.cpp

--- Everything/ASD/5.Binarne/Algorithms.cpp
+++ Everything/ASD/5.Binarne/Algorithms.cpp
@@ -67,92 +67,150 @@ auto muchBetterBinSearch(std::vector<T> &E, T x) -> int
 
 
 template<typename T>
-int hoarePartition(std::vector<T> &E, int k, int i, int j, int operacja)
+void HoarePartition(std::vector<T> &E, int k)
 {
     int m = 0;
-    int tmp = 0;
-    int n = j - i;
+    int operation = 1;
+    int i = 0;
+    int j = E.size();
 
-    std::cout << "HOARE PARTITION operacja numer:  " << operacja << std::endl;
-
-    m = partition(E, i, j);
-    std::cout << "m wynosi: " << m << std::endl <<  std::endl;
-
-    if (n - m == k)
+    while (j - m != k && j - i > 0)
     {
-        return m;
-    }
-    else
-    {
-        if (n - m > k)
+        std::cout << "METODA HOARE PARTITION: " << operation << " I SZUKAMY ELEMENT " << k << " CO DO WIELKOŚCI" << std::endl << "ZAKRES PRZUKANIA TABLICY: " << i << "-" << j << std::endl;
+        printArrayFromTo(E, i, j);
+        m = partition(E, i, j);
+        printArrayFromTo(E, i, j);
+        std::cout << "PO WYKONANIU PARTITION DLA OPERACJI: " << operation << " MEDIANA JEST NA POZYCJI: " << m << " I WYNOSI: " << E.at(m) << std::endl << std::endl;
+        if (j - m > k)
         {
-            std::cout << "PRZESZUKIWANY OBSZAR PRAWEJ CZESCI TABLICY DLA OPERACJI NUMER : " << operacja << std::endl;
-            for (int z = m + 1; z < n; z++)
-                std::cout << E.at(z) << ", ";
-            std::cout << std::endl;
-            tmp = m + 1 + hoarePartition(E, m + 1, n, k, operacja + 1);
+            i = m + 1;
         }
-        else
+        else if (j - m < k)
         {
-            std::cout << "PRZESZUKIWANY OBSZAR LEWEJ CZESCI TABLICY DLA OPERACJI NUMER : " << operacja << std::endl;
-            for (int z = i; z < m; z++)
-                std::cout << E.at(z) << ", ";
-            std::cout << std::endl;
-            tmp = hoarePartition(E, i, m - 1, k - (n - m), operacja + 1);
+            k -= (j - m);
+            j = m;
         }
+        operation++;
     }
-    return tmp;
+    std::cout << "KOIEC METODY HOARE PARTITION. ELEMENT " << k << " CO DO WIELKOŚCI WYNOSI " << E.at(m) << std::endl;
 }
 
+
 template<typename T>
-int hoareSplit(std::vector<T> &E, int k, int i, int j)
+void HoareSplit(std::vector<T> &E, int k)
 {
     int m = 0;
-    int tmp = 0;
-    int n = j - i;
+    int operation = 1;
+    int i = 0;
+    int j = E.size();
 
-    m = split(E);
-    std::cout << "m wynosi: " << m << std::endl;
-
-
-    if (n - m == k)
+    while (j - m != k && j - i > 0)
     {
-        return m;
-    }
-    else
-    {
-        if (n - m > k)
+
+        std::cout << "METODA HOARE SPLIT: " << operation << " I SZUKAMY ELEMENT " << k << " CO DO WIELKOŚCI" << std::endl << "ZAKRES PRZUKANIA TABLICY: " << i << "-" << j << std::endl;
+        printArrayFromTo(E, i, j);
+        m = split(E, i, j);
+        printArrayFromTo(E, i, j);
+        std::cout << "PO WYKONANIU SPLIT DLA OPERACJI: " << operation << " MEDIANA JEST NA POZYCJI: " << m << " I WYNOSI: " << E.at(m) << std::endl << std::endl;
+        if (j - m > k)
         {
-            tmp = m + 1 + hoareSplit(E, k,m + 1, n - 1);
+            i = m + 1;
         }
-        else
+        else if (j - m < k)
         {
-            tmp = hoareSplit(E, 0, m - 1, k - (n-m));
+            k -= (j - m);
+            j = m;
         }
+        operation++;
     }
-    return tmp;
+    std::cout << "KOIEC METODY HOARE SPLIT. ELEMENT " << k << " CO DO WIELKOŚCI WYNOSI " << E.at(m) << std::endl;
 }
 
 template<typename T>
 void SelectionSort(std::vector<T> &E)
 {
+    std::cout << "SELECTION SORT\n\n";
+    int operation = 0;
+    std::cout << operation++ << ":    ";
+    print(E);
     int i = 0;
     int j = 0;
     int min = 0;
+    int swapy = 0;
+    int porownania = 0;
 
     for (i = 0; i < E.size() - 1; i++)
     {
         min = i;
         for (j = i + 1; j < E.size(); j++)
+        {
             if (E.at(j) < E.at(min))
+            {
                 min = j;
+                porownania++;
+            }
+        }
         if (min != i)
+        {
+            std::cout << operation << ":    zamieniam liczby na indexach: " << min << " i " << i << std::endl << operation << ":    ";
+            printArrayFromToInColour(E, 0, E.size(), min, i);
             swap(E, min, i);
+            swapy++;
+            std::cout << operation++ << ":    ";
+            print(E);
+            std::cout << std::endl;
+        }
+        else
+        {
+            std::cout << operation << ":    Nie zmieniam zadnych liczb\n";
+            std::cout << operation++ << ":    ";
+            printArrayFromToHighlightOneElement(E, 0, E.size(), i);
+            std::cout << std::endl;
+        }
     }
+
+    std::cout << "LICZBA POROWNAN: " << porownania << " LICZBA SWAPOW: " << swapy << std::endl;
 }
 
 template<typename T>
 void InsertionSort(std::vector<T> &E)
 {
+    std::cout << "INSERTION SORT\n\n";
+    int i = 0;
+    int j = 0;
+    int operation = 0;
+    int porownania = 0;
+    int swapy = 0;
 
+    std::cout << operation << ":    ";
+    printArrayFromToHighlightOneElement(E, 0, E.size(), operation);
+
+    for (i = 1; i < E.size(); i++)
+    {
+        j = i;
+        if (E.at(j - 1) >  E.at(j))
+        {
+            while ((j > 0) && (E.at(j - 1) > E.at(j)))
+            {
+                std::cout << operation << ":    zamieniam liczby na indexach: " << j - 1 << " i " << j << std::endl << operation << ":    ";
+                printArrayFromToInColour(E, 0, E.size(), j - 1, j);
+                swap(E, j - 1, j);
+                swapy++;
+                porownania++;
+                std::cout << operation << ":    ";
+                print(E);
+                j--;
+            }
+        }
+        else
+        {
+            std::cout << operation << ":    niczego nie zmieniam\n";
+            std::cout << operation << ":    ";
+            printArrayFromToHighlightOneElement(E, 0, E.size(), operation);
+            porownania++;
+        }
+        operation++;
+        std::cout << std::endl;
+    }
+    std::cout << "LICZBA POROWNAN: " << porownania << " LICZBA SWAPOW:  " << swapy << std::endl;
 }

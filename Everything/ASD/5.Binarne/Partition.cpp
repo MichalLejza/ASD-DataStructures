c95c47d5e35b3feb3b37a8db3221f851adc56f44
Michał Lejza <101932740+MichalLejza@users.noreply.github.com>
2023-12-04 18:53:30 +0100

    Update Partition.cpp

--- Everything/ASD/5.Binarne/Partition.cpp
+++ Everything/ASD/5.Binarne/Partition.cpp
@@ -6,62 +6,58 @@
 
 
 template<typename T>
+void printArrayFromTo(std::vector<T> &E, int i, int j)
+{
+    for (int l = i; l < j; l++)
+        std::cout << E.at(l) << ", ";
+    std::cout << std::endl;
+}
+
+template<typename T>
+void printArrayFromToInColour(std::vector<T> &E, int i, int j, int indexOne, int indexTwo)
+{
+    for (int k = i; k < j; k++)
+    {
+        if (k == indexOne || k == indexTwo)
+            std::cout << "\033[32m" << E.at(k) << "\033[0m" << ", ";
+        else
+            std::cout << E.at(k) << ", ";
+    }
+    std::cout << std::endl;
+}
+
+template<typename T>
+void printArrayFromToHighlightOneElement(std::vector<T> &E, int i, int j, int index)
+{
+    for (int k = i; k < j; k++)
+    {
+        if (k == index)
+            std::cout << "\033[32m" << E.at(k) << "\033[0m" << ", ";
+        else
+            std::cout << E.at(k) << ", ";
+    }
+    std::cout << std::endl;
+}
+
+template<typename T>
 int partition(std::vector<T> &E, int i, int j)
 {
     int l = i - 1;
     int r = i;
     int n = j;
-    int operation = 1;
-
-    std::cout << std::endl << "METODA PARTITION: " << std::endl;
 
     while (r < n - 1)
     {
         if (E.at(r) < E.at(n - 1))
         {
             if (r > l + 1)
-            {
-                std::cout << operation << ":   zamieniam liczby na indexach: " << l + 1 << " i " << r << std::endl << operation << ":   ";
-                for (int k = i; k < j; k++)
-                {
-                    if (k == l + 1 || k == r)
-                        std::cout << "\033[32m" << E.at(k) << "\033[0m" << ", ";
-                    else
-                        std::cout << E.at(k) << ", ";
-                }
                 swap(E, r, l + 1);
-                std::cout << operation << ":   " ;
-                for (int k = i; k < j; k++)
-                {
-                    std::cout << E.at(k) << ", ";
-                }
-                std::cout << std::endl;
-                operation++;
-            }
             l++;
         }
         r++;
     }
     if (l + 1 < n - 1)
-    {
-        std::cout << operation << ":   dodatkowo zamieniam liczby na indexach: " << l + 1 << " i " << n - 1 << std::endl << operation << ":   ";
-        for (int k = i; k < j; k++)
-        {
-            if (k == l + 1 || k == n - 1)
-                std::cout << "\033[32m" << E.at(k) << "\033[0m" << ", ";
-            else
-                std::cout << E.at(k) << ", ";
-        }
         swap(E, l + 1, n - 1);
-        std::cout << std::endl << operation << ":   " ;
-        for (int k = i; k < j; k++)
-        {
-            std::cout << E.at(k) << ", ";
-        }
-
-    }
-    std::cout << std::endl << "tzw. mediana jest na pozycji:   " << l + 1 << "   i wynosi:   " << E.at(l + 1) << std::endl;
-    std::cout << std::endl;
     return l + 1;
 }
 
@@ -71,54 +67,21 @@ int split(std::vector<T> &E, int i, int j)
     int l = i + 1;
     int r = j - 1;
     int m = i;
-    int operation = 1;
 
-    std::cout << std::endl << "METODA SPLIT: " << std::endl;
-
-    while (l < r)
+    while (l <= r)
     {
-        if (r >= 0 && E.at(r) > E.at(m))
+        if (l <= r && E.at(r) > E.at(m))
             r--;
-        else if(E.at(l) < E.at(m) && l < r)
+        else if(E.at(l) < E.at(m) && l <= r)
             l++;
         else if (l < r)
         {
-            std::cout << operation << ":   zamieniam liczby na indexach: " << l << " i " << r << std::endl << operation << ":   ";
-            for (int k = i; k < j; k++)
-            {
-                if (k == l || k == r)
-                    std::cout << "\033[32m" << E.at(k) << "\033[0m" << ", ";
-                else
-                    std::cout << E.at(k) << ", ";
-            }
-            std::cout << std::endl << operation << ":   ";
             swap(E, l, r);
-            for (int k = i; k < j; k++)
-            {
-                std::cout << E.at(k) << ", ";
-            }
-            std::cout << std::endl;
-            operation++;
             r--;
             l++;
         }
     }
-    std::cout << operation << ": zamieniam liczby na indexach: " << m << " i " << r << std::endl << operation << ":   ";
-    for (int k = i; k < j; k++)
-    {
-        if (k == m || k == r)
-            std::cout << "\033[32m" << E.at(k) << "\033[0m" << ", ";
-        else
-            std::cout << E.at(k) << ", ";
-    }
-    swap(E, m, r);
-    std::cout << std::endl << operation << ":   ";
-    for (int k = i; k < j; k++)
-    {
-        std::cout << E.at(k) << ", ";
-    }
-    std::cout << std::endl << "tzw. mediana jest na pozycji:   " << r << "   i wynosi:   " << E.at(r) << std::endl;
-    std::cout <<  std::endl;
+    if (r > 0)
+        swap(E, m, r);
     return r;
 }
-

a4ccb5d57c5304c345d3900f1f0c801f1ea0156c
Michał Lejza <101932740+MichalLejza@users.noreply.github.com>
2023-12-19 09:09:18 +0100

    Delete Everything directory

--- Everything/ASD/9.Kolokwium2/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include "/Users/michallejza/CLionProjects/Everything/ASD/9.Kolokwium2/Algorithms.hpp"
-
-int main()
-{
-    std::vector<int> E = {2,6,1,0,14,7,16,18,11,13,9};
-    HoareSplit(E, 4);
-    return 0;
-}
